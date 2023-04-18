e292215cd44252f5101c31c0e6140ad84db47e9f
Juan <116412382+jsf97@users.noreply.github.com>
2023-04-18 19:14:35 -0300

    agregada funcion para agregar nuevos platos.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,24 @@
+import firstImg from './menu0.jpg'
+
+function addMenu(container, img, dish, price) {
+  const newMenu = document.createElement('div');
+  newMenu.classList.add('menu');
+
+  const newImg = document.createElement('img');
+  newImg.src = img;
+
+  const dishName = document.createElement('h2');
+  dishName.textContent = dish; 
+  
+  const dishPrice = document.createElement('p');
+  dishPrice.textContent = price;
+
+  newMenu.appendChild(newImg)
+  newMenu.appendChild(dishName);
+  newMenu.appendChild(dishPrice);
+
+  container.appendChild(newMenu)
+}
 
 export default function menuContent(append) {
   const container = document.createElement('div');
@@ -8,22 +29,19 @@ export default function menuContent(append) {
   title.textContent = 'Menu';
   container.appendChild(title);
 
-  const choice = document.createElement('h2');
-  choice.classList.add('menu-choice');
-  choice.textContent = 'Almuerzos';
-  container.appendChild(choice);
+  const dinner = document.createElement('h2');
+  dinner.classList.add('dinner')
+  dinner.textContent = 'Dinner'
+  container.appendChild(dinner)
 
-  const choiceContent = document.createElement('div');
-  choiceContent.classList.add('menu-choice__content');
-  container.appendChild(choiceContent);
-  for(let i = 0; i <= 5; i++) {
-    const lunchContainer = document.createElement('div');
-    lunchContainer.classList.add('lunch-container')
-    lunchContainer.textContent = 'asd'
-    choiceContent.appendChild(lunchContainer)
-  }
 
-  choiceContent.children[0].textContent = 'a'
+  // MENUES
+  const menuesContainer = document.createElement('div');
+  menuesContainer.classList.add('menuContainer');
+  container.appendChild(menuesContainer);
+
+  addMenu(menuesContainer, firstImg, 'pescado', '$10')
+  
 
   append.appendChild(container);
 }
